6341923f4253ab2ac9ba193bee67a953b10e323e
aldjanaliev <aldjanaliev@gmail.com>
2024-05-13 14:45:44 +0600

    NotFound

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,8 +1,15 @@
+import { Link } from 'react-router-dom';
 import './NotFound.css';
 
 function NotFound() {
   return (
-    <>NotFound</>
+    <>
+      <div className='NotFound'>404</div>
+      <p className="NotFound_txt">
+        Oops, go to 
+        <Link to="/">Home</Link>
+      </p>
+    </>
   );
 }
 
